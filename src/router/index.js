74d3b06e0547fe2de9fe5ea2e898d8b6863ed74e
LIYIFEI-0912 <18516305764@163.com>
2020-10-23 14:56:41 +0800

    Vue储留箱项目

--- src/router/index.js
+++ src/router/index.js
@@ -20,11 +20,6 @@ Vue.use(Router)
 export default new Router({
   routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/login',
       name: 'login',
       component: Login
     },
